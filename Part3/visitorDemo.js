6aaba7bcdc717da0e15435d3bccfaaa8cc0a3db3
Wize998 <120222039+Wize998@users.noreply.github.com>
2024-12-06 11:32:32 -0500

    Update visitorDemo.js

--- Part3/visitorDemo.js
+++ Part3/visitorDemo.js
@@ -1,3 +1,32 @@
+var carVariable = function()
+{
+var seats = 5;
+var doors = 4;
+this.accept = function(visitorObject)
+{
+visitorObject.visit(this);
+}
+}
+var truckVariable = function()
+{
+var towPackage = true;
+var doors = 2;
+this.accept = function(visitorObject)
+{
+visitorObject.visit(this);
+}
+}
+var monsterTruckVariable = function()
+{
+var looksLikeADragon = true;
+var doors = 1.5;
+this.accept = function(visitorObject)
+{
+visitorObject.visit(this);
+}
+}
+  
+
 var CarVisitor = function()
 {
 var visit = function(carVariable)
